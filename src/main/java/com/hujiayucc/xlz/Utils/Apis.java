8fb34e197731dd8ced8b21b051a1e3fb1db37f96
hujiayucc <hujiayucc@qq.com>
2022-08-15 01:37:18 +0800

    新增若干API调用回显

--- src/main/java/com/hujiayucc/xlz/Utils/Apis.java
+++ src/main/java/com/hujiayucc/xlz/Utils/Apis.java
@@ -16,48 +16,374 @@ public class Apis implements Api, Type, Color, Url {
     @Autowired
     private Lib lib;
 
-    private static Map<String, Object> param = new HashMap<String, Object>();
-    private static String ret;
-    private static String Log = "log";
-    private static String Textclr = "textclr";
-    private static String Bgclr = "bgclr";
-    private static String Robot = "logonqq";
-    private static String Group = "group";
-    private static String QQ = "toqq";
-    private static String Msg = "msg";
-    private static String Type = "type";
-    private static String Anonymous = "anonymous";
+    private static String ret = "null";
 
     @Override
     public String 输出日志(String log, int textColor, int bgColor) {
-        param.put(Log, log);
-        param.put(Textclr, textColor);
-        param.put(Bgclr, bgColor);
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("log", log);
+        param.put("textclr", textColor);
+        param.put("bgclr", bgColor);
         ret = lib.POST(输出日志, param);
-        param.clear();
         return ret;
     }
 
     @Override
     public String 发送好友消息(Long robot, Long qq, String msg, String type) {
-        param.put(Robot, robot);
-        param.put(QQ, qq);
-        param.put(Msg, msg);
-        param.put(Type, type);
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("logonqq", robot);
+        param.put("toqq", qq);
+        param.put("msg", msg);
+        param.put("type", type);
         ret = lib.POST(发送好友消息, param);
-        param.clear();
         return ret;
     }
 
     @Override
     public String 发送群消息(Long robot, Long group, String msg, String type, boolean anonymous) {
-        param.put(Robot, robot);
-        param.put(Group, group);
-        param.put(Msg, msg);
-        param.put(Type, type);
-        param.put(Anonymous, String.valueOf(anonymous));
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("logonqq", robot);
+        param.put("group", group);
+        param.put("msg", msg);
+        param.put("type", type);
+        param.put("anonymous", String.valueOf(anonymous));
         ret = lib.POST(发送群消息, param);
-        param.clear();
+        return ret;
+    }
+
+    @Override
+    public String 发送群临时消息(Long robot, Long group, Long qq, String msg) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("logonqq", robot);
+        param.put("group", group);
+        param.put("toqq", qq);
+        param.put("msg", msg);
+        ret = lib.POST(发送群临时消息, param);
+        return ret;
+    }
+
+    @Override
+    public String 发送讨论组消息(Long robot, Long discussionid, String msg, String type) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("logonqq", robot);
+        param.put("discussionid", discussionid);
+        param.put("msg", msg);
+        param.put("type", type);
+        ret = lib.POST(发送讨论组消息, param);
+        return ret;
+    }
+
+    @Override
+    public String 发送讨论组临时消息(Long robot, Long discussionid, Long qq, String msg) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("logonqq", robot);
+        param.put("discussionid", discussionid);
+        param.put("toqq", qq);
+        param.put("msg", msg);
+        ret = lib.POST(发送讨论组临时消息, param);
+        return ret;
+    }
+
+    @Override
+    public String 添加好友(Long robot, Long qq, String msg, String remark) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("logonqq", robot);
+        param.put("toqq", qq);
+        param.put("msg", msg);
+        param.put("remark", remark);
+        ret = lib.POST(添加好友, param);
+        return ret;
+    }
+
+    @Override
+    public String 添加群(Long robot, Long group, String msg) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("msg", msg);
+        lib.POST(添加群, param);
+        return ret;
+    }
+
+    @Override
+    public String 删除好友(Long robot, Long qq) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        ret = lib.POST(删除好友, param);
+        return ret;
+    }
+
+    @Override
+    public String 置屏蔽好友(Long robot, Long qq, boolean isblock) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        param.put("isblock", String.valueOf(isblock));
+        ret = lib.POST(置屏蔽好友, param);
+        return ret;
+    }
+
+    @Override
+    public String 置特别关心好友(Long robot, Long qq, boolean iscare) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        param.put("iscare", String.valueOf(iscare));
+        ret = lib.POST(置特别关心好友, param);
+        return ret;
+    }
+
+    @Override
+    public String 上传好友图片(Long robot, Long qq, String pic, boolean isflash, String type, boolean isgif) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        param.put("pic", pic);
+        param.put("isflash", String.valueOf(isflash));
+        param.put("type", type);
+        param.put("isgif", String.valueOf(isgif));
+        ret = lib.POST(上传好友图片, param);
+        return ret;
+    }
+
+    @Override
+    public String 上传群图片(Long robot, Long group, String pic, boolean isflash, String type, boolean isgif) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("pic", pic);
+        param.put("isflash", String.valueOf(isflash));
+        param.put("type", type);
+        param.put("isgif", isgif);
+        ret = lib.POST(上传群图片, param);
+        return ret;
+    }
+
+    @Override
+    public String 上传好友语音(Long robot, Long qq, int audiotype, String text, String type, String audio, int time) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        param.put("audiotype", audiotype);
+        param.put("text", text);
+        param.put("type", type);
+        param.put("audio", audio);
+        param.put("time", time);
+        ret = lib.POST(上传好友语音, param);
+        return ret;
+    }
+
+    @Override
+    public String 上传群语音(Long robot, Long group, int audiotype, String text, String type, String audio, int time) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("audiotype", audiotype);
+        param.put("text", text);
+        param.put("type", type);
+        param.put("audio", audio);
+        param.put("time", time);
+        ret = lib.POST(上传群语音, param);
+        return ret;
+    }
+
+    @Override
+    public String 上传头像(Long robot, String type, String pic) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("type", type);
+        param.put("pic", pic);
+        ret = lib.POST(上传头像, param);
+        return ret;
+    }
+
+    @Override
+    public String 设置群名片(Long robot, Long group, Long qq, String newcard) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("toqq", qq);
+        param.put("newcard", newcard);
+        ret = lib.POST(设置群名片, param);
+        return ret;
+    }
+
+    @Override
+    public String 取昵称(Long robot, Long qq, boolean fromcache) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        param.put("fromcache", fromcache);
+        ret = lib.POST(取昵称, param);
+        return ret;
+    }
+
+    @Override
+    public String 获取skey(Long robot) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        ret = lib.POST(获取skey, param);
+        return ret;
+    }
+
+    @Override
+    public String 获取pskey(Long robot, String domain) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("domain", domain);
+        ret = lib.POST(获取pskey, param);
+        return ret;
+    }
+
+    @Override
+    public String 获取clientkey(Long robot) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        ret = lib.POST(获取clientkey, param);
+        return ret;
+    }
+
+    @Override
+    public String 取框架QQ() {
+        Map<String, Object> param = new HashMap<String, Object>();
+        ret = lib.POST(取框架QQ, param);
+        return ret;
+    }
+
+    @Override
+    public String 取好友列表(Long robot) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        ret = lib.POST(取好友列表, param);
+        return ret;
+    }
+
+    @Override
+    public String 取群列表(Long robot) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        ret = lib.POST(取群列表, param);
+        return ret;
+    }
+
+    @Override
+    public String 取群成员列表(Long robot, Long group) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        ret = lib.POST(取群成员列表, param);
+        return ret;
+    }
+
+    @Override
+    public String 设置管理员(Long robot, Long group, Long qq, boolean unadmin) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("toqq", qq);
+        param.put("unadmin", String.valueOf(unadmin));
+        ret = lib.POST(设置管理员, param);
+        return ret;
+    }
+
+    @Override
+    public String 取群管理层列表(Long robot, Long group) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        ret = lib.POST(取群管理层列表, param);
+        return ret;
+    }
+
+    @Override
+    public String 取群名片(Long robot, Long group, Long qq) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("toqq", qq);
+        ret = lib.POST(取群名片, param);
+        return ret;
+    }
+
+    @Override
+    public String 取个性签名(Long robot, Long qq) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("toqq", qq);
+        ret = lib.POST(取个性签名, param);
+        return ret;
+    }
+
+    @Override
+    public String 修改昵称(Long robot, String nickname) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("nickname", nickname);
+        ret = lib.POST(修改昵称, param);
+        return ret;
+    }
+
+    @Override
+    public String 修改个性签名(Long robot, String signat, String pos) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("signat", signat);
+        param.put("pos", pos);
+        ret = lib.POST(修改个性签名, param);
+        return ret;
+    }
+
+    @Override
+    public String 删除群成员(Long robot, Long group, Long qq, boolean ignoreaddgrequest) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("toqq", qq);
+        param.put("ignoreaddgrequest", String.valueOf(ignoreaddgrequest));
+        ret = lib.POST(删除群成员, param);
+        return ret;
+    }
+
+    @Override
+    public String 禁言群成员(Long robot, Long group, Long qq, int time) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("toqq", qq);
+        param.put("time", time);
+        ret = lib.POST(禁言群成员, param);
+        return ret;
+    }
+
+    @Override
+    public String 退群(Long robot, Long group, boolean dismiss) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("dismiss", String.valueOf(dismiss));
+        ret = lib.POST(退群, param);
+        return ret;
+    }
+
+    @Override
+    public String 上传群头像(Long robot, Long group, String type, String pic) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("type", type);
+        param.put("pic", pic);
+        ret = lib.POST(上传群头像, param);
+        return ret;
+    }
+
+    @Override
+    public String 全员禁言(Long robot, Long group, boolean ismute) {
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("robot", robot);
+        param.put("group", group);
+        param.put("ismute", String.valueOf(ismute));
+        ret = lib.POST(全员禁言, param);
         return ret;
     }
 }
